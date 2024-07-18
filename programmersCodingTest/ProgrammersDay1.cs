a3f1f65313f1d61698eaf29e1b6155bba8385882
februarywind <februarywind0@gmail.com>
2024-07-18 17:48:53 +0900

    codingTest Day1
    분수의 덧셈은 너무 어려운데 내가 수학을 못해서 그런가 다음에 풀어야지

---
+++ programmersCodingTest/ProgrammersDay1.cs
@@ -0,0 +1,27 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDay1
+    {
+        //프로그래머스 입문 Day1
+        public int solution1(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            return answer;
+        }
+        public int solution2(int num1, int num2)
+        {
+            int answer = num1 - num2;
+            return answer;
+        }
+        public int solution3(int num1, int num2)
+        {
+            int answer = num1 * num2;
+            return answer;
+        }
+        public int solution4(int num1, int num2)
+        {
+            int answer = num1 / num2;
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDay2.cs
@@ -0,0 +1,30 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDay2
+    {
+        //프로그래머스 입문 Day2
+        public int solution1(float num1, float num2)
+        {
+            int answer = (int)((num1 / num2) * 1000);
+            return answer;
+        }
+        public int solution2(int num1, int num2)
+        {
+            int answer = 0;
+            if (num1 == num2)
+                answer = 1;
+            else
+                answer = -1;
+            return answer;
+        }
+
+        public int[] solution4(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] *= 2;
+            }
+            return numbers;
+        }
+    }
+}
