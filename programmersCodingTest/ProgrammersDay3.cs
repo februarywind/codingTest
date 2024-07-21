5a18a0ffbfe6539c77bd46cc0a19e60843ff6ae1
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-21 15:22:09 +0900

    ProgrammersDay4
    3일차랑 4일차 문제를 풀었따

---
+++ programmersCodingTest/ProgrammersDay3.cs
@@ -0,0 +1,49 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDay3
+    {
+        //프로그래머스 입문 Day2
+        public int solution1(int num1, int num2) // 나머지 구하기
+        {
+            int answer = num1 % num2;
+            return answer;
+        }
+        public int solution2(int[] array) // 중앙값 구하기
+        {
+            Array.Sort(array);
+            int answer = array[array.Length / 2];
+            return answer;
+        }
+        public int solution3(int[] array) // 최빈값 구하기 정렬하고 LastIndexOf로 해당 원소의 가장 마지막 위치 구해서 계산
+        {
+            Array.Sort(array);
+            int count = 0;
+            int num = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (count < Array.LastIndexOf(array, array[i]) - i + 1)
+                {
+                    count = Array.LastIndexOf(array, array[i]) - i + 1;
+                    num = array[i];
+                }
+                else if (count == Array.LastIndexOf(array, array[i]) - i + 1)
+                {
+                    num = -1;
+                }
+                i = Array.LastIndexOf(array, array[i]);
+            }
+            return num;
+        }
+        public int[] solution4(int n) // 짝수는 싫어요
+        {
+            int[] answer = new int[1];
+            for (int i = 1; i <= n; i += 2)
+            {
+                answer[answer.Length - 1] = i;
+                Array.Resize(ref answer, answer.Length + 1);
+            }
+            Array.Resize(ref answer, answer.Length - 1);
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDay4.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+
+namespace programmersCodingTest
+{
+    internal class ProgrammersDay4
+    {
+        //프로그래머스 입문 Day4
+        public int solution1(int n) // 피자 나눠 먹기 (1)
+        {
+            int answer = n / 7;
+            if (n % 7 != 0)
+            {
+                answer++;
+            }
+            return answer;
+        }
+        public int solution2(int n) // 피자 나눠 먹기 (2)
+        {
+            int answer = 6;
+            while (answer % n != 0)
+            {
+                answer += 6;
+            }
+            return answer / 6;
+        }
+        public int solution3(int slice, int n) // 피자 나눠 먹기 (3)
+        {
+            int answer = 1;
+            if (n - slice > 0)
+            {
+                answer += (n - slice) / slice;
+                if ((n - slice) % slice != 0)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+        public double solution4(int[] numbers) // 배열의 평균값
+        {
+            double answer = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer += numbers[i];
+            }
+            answer /= numbers.Length;
+            return answer;
+        }
+    }
+}
