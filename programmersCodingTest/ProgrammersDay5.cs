7dedd7713d1fe87f75cc61a2b8fc6e7537ef8a27
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-22 14:10:48 +0900

    day5, 6
    쉬웠다

---
+++ programmersCodingTest/ProgrammersDay5.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace programmersCodingTest
+{
+    internal class ProgrammersDay5
+    {
+        //프로그래머스 입문 Day5
+        public int solution1(int price) // 옷가게 할인 받기
+        {
+            if (price >= 500000)
+            {
+                price = (int)(price * 0.8);
+            }
+            else if (price >= 300000)
+            {
+                price = (int)(price * 0.9);
+            }
+            else if (price >= 100000)
+            {
+                price = (int)(price * 0.95);
+            }
+
+            return price;
+        }
+        public int[] solution2(int money) // 아이스 아메리카노
+        {
+            int[] answer = new int[2];
+            answer[0] = money / 5500;
+            answer[1] = money % 5500;
+            return answer;
+        }
+        public int solution3(int age) // 나이 출력
+        {
+            int answer = 2023 - age;
+            return answer;
+        }
+        public int[] solution4(int[] num_list) // 배열 뒤집기
+        {
+            Array.Reverse(num_list);
+            return num_list;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDay6.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+
+namespace programmersCodingTest
+{
+    internal class ProgrammersDay6
+    {
+        //프로그래머스 입문 Day6
+        public string solution1(string my_string) // 문자열 뒤집기
+        {
+            string answer = "";
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                answer += my_string[my_string.Length - 1 - i];
+            }
+            return answer;
+        }
+        public void solution2(int n) // 직각삼각형 출력하기
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public int[] solution3(int[] num_list) // 짝수 홀수 개수
+        {
+            int[] answer = new int[2];
+            foreach (var item in num_list)
+            {
+                if (item % 2 == 0)
+                {
+                    answer[0]++;
+                }
+                else
+                {
+                    answer[1]++;
+                }
+            }
+            return answer;
+        }
+        public string solution4(string my_string, int n) // 문자 반복 출력하기
+        {
+            string answer = "";
+            foreach (var item in my_string)
+            {
+                for (int i = 0; i < n; i++)
+                    answer += item;
+            }
+            return answer;
+        }
+    }
+}
