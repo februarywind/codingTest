fc4bf934f14af7b51d1f224c75e3d2e94884a306
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-23 18:15:21 +0900

    day7, 8
    아 배고프다 힘들어

---
+++ programmersCodingTest/ProgrammersDay7.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+namespace programmersCodingTest
+{
+    internal class ProgrammersDay7
+    {
+        //프로그래머스 입문 Day7
+        public string solution1(string my_string, string letter) // 특정 문자 제거하기
+        {
+            string answer = "";
+            char _letter = char.Parse(letter); // 제거할 문자를 char으로 변경
+            foreach (var item in my_string)
+            {
+                if (item != _letter)
+                {
+                    answer += item;
+                }
+            }
+            return answer;
+        }
+        public int solution2(int angle) // 각도기
+        {
+            if (angle == 90)
+                return 2;
+            else if (angle == 180)
+                return 4;
+            else if (angle < 90)
+                return 1;
+            else
+                return 3;
+        }
+        public int solution3(int n, int k) // 양꼬치 
+        {
+            int answer = (12000 * n) + (2000 * k) - ((n / 10) * 2000);
+            return answer;
+        }
+        public int solution4(int n) // 짝수의 합
+        {
+            int answer = 0;
+            for (int i = 0; i <= n; i += 2)
+            {
+                answer += i;
+            }
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDay8.cs
@@ -0,0 +1,58 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDay8
+    {
+        //프로그래머스 입문 Day8
+        public int[] solution1(int[] numbers, int num1, int num2) // 배열 자르기
+        {
+            int[] answer = new int[num2 - num1 + 1];
+            Array.Copy(numbers, num1, answer, 0, num2 - num1 + 1);
+            return answer;
+        }
+        public string solution2(int age) // 외계행성의 나이
+        {
+            string ageString = age.ToString();
+            string answer = "";
+            foreach (var item in ageString)
+            {
+                int a = item - '0';
+                char b = 'a';
+                for (int i = 0; i < a; i++)
+                {
+                    b++;
+                }
+                answer += b;
+            }
+            return answer;
+        }
+        public int[] solution3(int[] emergency) // 진료순서 정하기
+        {
+            int[] answer = new int[emergency.Length];
+            Array.Copy(emergency, answer, emergency.Length);
+            Array.Sort(emergency);
+            Array.Reverse(emergency);
+            int temp = 0;
+            foreach (var item in emergency)
+            {
+                temp++;
+                answer[Array.IndexOf(answer, item)] = temp * 111;
+            }
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] /= 111;
+            }
+
+            return answer;
+        }
+        public int solution4(int n) // 순서쌍의 개수
+        {
+            int answer = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                    answer++;
+            }
+            return answer;
+        }
+    }
+}
