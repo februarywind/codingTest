7d203ff45ea82615395efa116c6e4a052f13bd6f
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-24 18:59:30 +0900

    dayy 9, 10
    솔루션 탐색기 정렬하는거 실화냐
    왜 1 다음에 10인데

---
+++ programmersCodingTest/ProgrammersDay9.cs
@@ -0,0 +1,76 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDay9
+    {
+        //프로그래머스 입문 Day9
+        public int solution1(int hp) // 개미 군단 
+        {
+            int answer = 0;
+            answer += hp / 5;
+            hp = hp % 5;
+            answer += hp / 3;
+            hp = hp % 3;
+            answer += hp;
+            return answer;
+        }
+        public string solution2(string letter) // 모스 부호
+        {
+            string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            string answer = "";
+            string answer1 = "";
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (letter[i] != ' ')
+                {
+                    answer += letter[i];
+                }
+                else
+                {
+                    answer1 += (char)('a' + Array.IndexOf(morse, answer));
+                    answer = "";
+                }
+            }
+            answer1 += (char)('a' + Array.IndexOf(morse, answer));
+            return answer1;
+        }
+        public string solution3(string rsp) // 가위 바위 보
+        {
+            string answer = "";
+            foreach (var item in rsp)
+            {
+                answer += (item == '2') ? "0" : (item == '0') ? "5" : "2";
+            }
+            return answer;
+        }
+        public int solution4(int balls, int share) // 구슬을 나누는 경우의 수 이건 진짜 어떻게 푼건지도 모르겠다.
+        {
+            int[] a = new int[0];
+
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Array.Resize(ref a, a.Length + 1);
+                    int L = Array.IndexOf(a, 0);
+                    if (j == 0)
+                    {
+                        a[L] = i + 1;
+                        continue;
+                    }
+                    if (j == i)
+                    {
+                        a[L] = 1;
+                        continue;
+                    }
+                    a[L] = a[L - i] + a[L - i - 1];
+                }
+            }
+            int sum = 0;
+            for (int i = 1; i < balls; i++)
+            {
+                sum += i;
+            }
+            return (a[sum + share - 1]);
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDayy10.cs
@@ -0,0 +1,45 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy10
+    {
+        //프로그래머스 입문 Day10
+        public int solution1(int[] dot) // 점의 위치 구하기
+        {
+            bool xp = dot[0] > 0;
+            bool yp = dot[1] > 0;
+            int answer = (!xp && yp) ? 2 : (xp && yp) ? 1 : (!xp && !yp) ? 3 : 4;
+            return answer;
+        }
+        public int[,] solution2(int[] num_list, int n) // 2차원으로 만들기
+        {
+            int[,] answer = new int[num_list.Length / n, n];
+            for (int i = 0; i < num_list.Length / n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    answer[i, j] = num_list[j + i * n];
+                }
+            }
+            return answer;
+        }
+        public int solution3(int[] numbers, int k) // 공 던지기
+        {
+            return numbers[((2 * (k - 1))) % numbers.Length];
+        }
+        public int[] solution4(int[] numbers, string direction) // 배열 회전시키기
+        {
+            int[] answer = new int[numbers.Length];
+            if (direction == "right")
+            {
+                answer[0] = numbers[numbers.Length - 1];
+                Array.Copy(numbers, 0, answer, 1, numbers.Length - 1);
+            }
+            else
+            {
+                answer[answer.Length - 1] = numbers[0];
+                Array.Copy(numbers, 1, answer, 0, numbers.Length - 1);
+            }
+            return answer;
+        }
+    }
+}
