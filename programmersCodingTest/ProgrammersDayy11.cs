a7817e47d01184f12b5ad1287d2aa0a963189c56
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-25 17:58:16 +0900

    day 11 12
    더워둬워두어둬우더워둬우더워더워더워

---
+++ programmersCodingTest/ProgrammersDayy11.cs
@@ -0,0 +1,46 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy11
+    {
+        //프로그래머스 입문 Day11
+        public int solution1(int[] box, int n) // 주사위의 개수
+        {
+            int answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
+            return answer;
+        }
+        public int solution2(int n) // 합성수 찾기
+        {
+            int answer = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
+        public int solution3(int[] numbers) // 최댓값 만들기
+        {
+            Array.Sort(numbers);
+            return numbers[numbers.Length - 2] * numbers[numbers.Length - 1];
+        }
+        public int solution4(int n) // 팩토리얼
+        {
+            int answer = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                answer *= i;
+                if (answer > n)
+                    return i - 1;
+                if (answer == n)
+                    return i;
+            }
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDayy12.cs
@@ -0,0 +1,75 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy12
+    {
+        //프로그래머스 입문 Day12
+        public string solution1(string my_string) // 모음 제거
+        {
+            char[] b = my_string.ToCharArray();
+            char[] c = { 'a', 'e', 'i', 'o', 'u' };
+            my_string = "";
+
+            foreach (var item in c)
+            {
+                int i = b.Count(x => x == item);
+                for (int j = 0; j < i; j++)
+                {
+                    b[Array.IndexOf(b, item)] = '@';
+                }
+            }
+            foreach (var item in b)
+            {
+                if (item != '@')
+                {
+                    my_string += item;
+                }
+            }
+
+            return my_string;
+        }
+        public int[] solution2(string my_string) // 문자열 정렬하기(1)
+        {
+            int[] answer = new int[0];
+            foreach (var item in my_string)
+            {
+                if (item - '0' >= 0 && item - '0' < 10)
+                {
+                    Array.Resize(ref answer, answer.Length + 1);
+                    answer[answer.Length - 1] = item - '0';
+                }
+            }
+            Array.Sort(answer);
+            return answer;
+        }
+        public int solution3(string my_string) // 숨어있는 숫자의 덧샘(1)
+        {
+            int answer = 0;
+            foreach (var item in my_string)
+            {
+                if (item - '0' >= 0 && item - '0' < 10)
+                {
+                    answer += item - '0';
+                }
+            }
+            return answer;
+        }
+        public int[] solution4(int n) // 소인수분해
+        {
+            int[] answer = new int[0];
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    n /= i;
+                    if (Array.IndexOf(answer, i) == -1)
+                    {
+                        Array.Resize(ref answer, answer.Length + 1);
+                        answer[answer.Length - 1] = i;
+                    }
+                    i = 1;
+                }
+            }
+            return answer;
+        }
+    }
+}
