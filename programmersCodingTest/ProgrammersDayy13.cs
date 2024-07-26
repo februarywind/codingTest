0a434725780e7d196a7aec5f41603e8604ed52b0
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-26 20:20:31 +0900

    13 14
    오늘은 별로 더워

---
+++ programmersCodingTest/ProgrammersDayy13.cs
@@ -0,0 +1,57 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy13
+    {
+        //프로그래머스 입문 Day13
+        public int solution1(string s) // 컨트롤 제트
+        {
+            string[] words = s.Split(' ');
+            int answer = 0;
+            int temp = 0;
+            int temp2 = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (int.TryParse(words[i], out temp))
+                {
+                    temp2 = temp;
+                    answer += temp;
+                }
+                else
+                {
+                    answer += temp2 * -1;
+                }
+            }
+            return answer;
+        }
+        public int[] solution2(string[] strlist) // 배열 원소의 길이
+        {
+            int[] answer = new int[strlist.Length];
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = strlist[i].Length;
+            }
+            return answer;
+        }
+        public string solution3(string my_string) // 중복된 문자 제거
+        {
+            string answer = "";
+            char[] chars = answer.ToCharArray();
+            foreach (var item in my_string)
+            {
+                if (Array.IndexOf(chars, item) == -1)
+                {
+                    answer += item;
+                    chars = answer.ToCharArray();
+                }
+            }
+            return answer;
+        }
+        public int solution4(int[] sides) //삼각형의 완성조건 (1)
+        {
+            int max = sides.Max(x => x);
+            int sum = sides.Sum(x => x) - max;
+            int answer = (max < sum) ? 1 : 2;
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDayy14.cs
@@ -0,0 +1,54 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy14
+    {
+        //프로그래머스 입문 Day14
+        public int solution1(int[] array, int n) // 가까운 수
+        {
+            int answer = 0;
+            int temp = 100;
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i] - n) < temp)
+                {
+                    temp = Math.Abs(array[i] - n);
+                    answer = array[i];
+                }
+            }
+            return answer;
+        }
+        public int solution2(int order) // 369게임
+        {
+            char[] b = order.ToString().ToCharArray();
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] -= '0';
+            }
+            return b.Count(x => x % 3 == 0 && x != 0);
+        }
+        public string solution3(string cipher, int code) // 암호 해독
+        {
+            string answer = "";
+            for (int i = code; i <= cipher.Length; i += code)
+            {
+                answer += cipher[i - 1];
+            }
+            return answer;
+        }
+        public string solution4(string my_string) // 대문자와 소문자
+        {
+            string answer = "";
+            foreach (var item in my_string)
+            {
+                if (item < 'a')
+                {
+                    answer += (char)(item + 32);
+                }
+                else
+                    answer += (char)(item - 32);
+            }
+            return answer;
+        }
+    }
+}
