f7a0651153a5c78c1d8109c46b0f800506a637e0
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-27 09:27:06 +0900

    15 16
    와 토요일이다

---
+++ programmersCodingTest/ProgrammersDayy15.cs
@@ -0,0 +1,65 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy15
+    {
+        //프로그래머스 입문 Day15
+        public long solution1(string numbers) // 영어가 싫어요
+        {
+            long answer = 0;
+            string number = "";
+            string[] answers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                number += numbers[i];
+                if (Array.IndexOf(answers, number) != -1)
+                {
+                    answer = answer * 10 + Array.IndexOf(answers, number);
+                    number = "";
+                }
+            }
+            return answer;
+        }
+        public string solution2(string my_string, int num1, int num2) // 인덱스 바꾸기
+        {
+            string answer = "";
+            char[] b = my_string.ToCharArray();
+            b[num1] = my_string[num2];
+            b[num2] = my_string[num1];
+            for (int i = 0; i < b.Length; i++)
+            {
+                answer += b[i];
+            }
+            return answer;
+        }
+        public string solution3(string s) // 한 번만 등장한 문자
+        {
+            string answer = "";
+            char[] b = s.ToCharArray().Distinct().ToArray();
+            List<char> list = new List<char>();
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (Array.IndexOf(s.ToCharArray(), b[i]) == Array.LastIndexOf(s.ToCharArray(), b[i]))
+                    list.Add(b[i]);
+            }
+            list.Sort();
+            foreach (var item in list)
+            {
+                answer += item;
+
+            }
+            return answer;
+        }
+        public int[] solution4(int n) // 약수 구하기
+        {
+            List<int> answer = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    answer.Add(i);
+                }
+            }
+            return answer.ToArray();
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDayy16.cs
@@ -0,0 +1,50 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy16
+    {
+        //프로그래머스 입문 Day16
+        public int solution1(string message) // 편지
+        {
+            return message.Length * 2;
+        }
+        public int[] solution2(int[] array) // 가장 큰 수 찾기
+        {
+            int[] answer = { array.Max(), Array.IndexOf(array, array.Max()) };
+            return answer;
+        }
+        public int solution3(string my_string) // 문자열 계산하기
+        {
+            List<string> list = my_string.Split(' ').ToList();
+            int space = list.Count(x => x == " ");
+            for (int i = 0; i <= space; i++)
+            {
+                list.Remove(" ");
+            }
+            int answer = int.Parse(list[0]);
+            for (int i = 2; i < list.Count(); i++)
+            {
+                if (list[i - 1] == "+")
+                {
+                    answer += int.Parse(list[i]);
+                }
+                else if (list[i - 1] == "-")
+                {
+                    answer -= int.Parse(list[i]);
+                }
+            }
+            return answer;
+        }
+        public int solution4(string[] s1, string[] s2) // 배열의 유사도
+        {
+            int answer = 0;
+            foreach (var item in s1)
+            {
+                if (Array.IndexOf(s2, item) != -1)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
