87d481fee683e009808bf1b69ea9ba56ca2fdea0
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-28 12:25:35 +0900

    17 18
    에어컨을 사기로 했다

---
+++ programmersCodingTest/ProgrammersDayy17.cs
@@ -0,0 +1,59 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy17
+    {
+        //프로그래머스 입문 Day17
+        public int solution1(int num, int k) // 숫자 찾기
+        {
+            char[] arr = num.ToString().ToCharArray();
+            int answer = Array.IndexOf(arr, (char)(k + '0'));
+            if (answer != -1)
+            {
+                answer++;
+            }
+            return answer;
+        }
+        public int[] solution2(int n, int[] numlist) // n의 배수 고르기
+        {
+            List<int> answer = new List<int>();
+            for (int i = 0; i < numlist.Length; i++)
+            {
+                if (numlist[i] % n == 0)
+                {
+                    answer.Add(numlist[i]);
+                }
+            }
+            return answer.ToArray();
+        }
+        public int solution3(int n) // 자릿수 더하기
+        {
+            int answer = 0;
+            string s = n.ToString();
+            foreach (var item in s)
+            {
+                answer += item - '0';
+            }
+            return answer;
+        }
+        public string[] solution4(string[] quiz) //OX퀴즈
+        {
+            string[] answer = new string[quiz.Length];
+            for (int i = 0; i < quiz.Length; i++)
+            {
+                string[] quiz2 = quiz[i].Split(' ');
+                int temp = -1;
+                if (quiz2[1] == "+")
+                    temp = 1;
+                if (int.Parse(quiz2[0]) + int.Parse(quiz2[2]) * temp == int.Parse(quiz2[4]))
+                {
+                    answer[i] = "O";
+                }
+                else
+                {
+                    answer[i] = "X";
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDayy18.cs
@@ -0,0 +1,55 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy18
+    {
+        //프로그래머스 입문 Day18
+        public int solution1(string str1, string str2) // 문자열안에 문자열
+        {
+            int answer = 0;
+            foreach (var item in str1)
+            {
+                if (str2[answer] == item)
+                    answer++;
+                else
+                    answer = 0;
+                if (answer == str2.Length)
+                {
+                    return 1;
+                }
+            }
+            return 2;
+        }
+        public int solution2(int n) // 제곱수 판별하기
+        {
+            for (int i = 1; i < 1001; i++)
+            {
+                if (i * i == n)
+                    return 1;
+            }
+            return 2;
+        }
+        public int solution3(int n, int t) // 세균 증식
+        {
+            for (int i = 1; i <= t; i++)
+            {
+                n *= 2;
+            }
+            return n;
+        }
+        public string solution4(string my_string) // 문자열 정렬하기 (2)
+        {
+            string answer = "";
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                if (my_string[i] < 'a')
+                    answer += (char)(my_string[i] + 32);
+                else
+                    answer += my_string[i];
+            }
+            char[] chars = answer.ToCharArray();
+            Array.Sort(chars);
+            answer = new string(chars);
+            return answer;
+        }
+    }
+}
