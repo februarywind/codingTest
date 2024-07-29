57b25c183e61903e5e0acb6b491c2677ca4fc987
februarywind <89524114+februarywind@users.noreply.github.com>
2024-07-29 16:58:03 +0900

    19 20
    아 머리가 아파아파아파아파

---
+++ programmersCodingTest/ProgrammersDayy19.cs
@@ -0,0 +1,48 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy19
+    {
+        //프로그래머스 입문 Day19
+        public int solution1(int[] array) // 7의 개수
+        {
+            string s = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                s += array[i];
+            }
+            return s.Count(x => x == '7');
+        }
+        public string[] solution2(string my_str, int n) // 잘라서 배열로 저장하기
+        {
+            string[] answer = new string[(my_str.Length % n == 0) ? my_str.Length / n : my_str.Length / n + 1];
+            for (int i = 0; i < answer.Length; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    answer[i] += my_str[j + i * n];
+                    if (j + i * n == my_str.Length - 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
+        public int solution3(int[] array, int n) // 중복된 숫자 개수
+        {
+            return array.Count(x => x == n);
+        }
+        public int solution4(int[] array, int height) // 머쓱이보다 키 큰 사람
+        {
+            int answer = 0;
+            foreach (var item in array)
+            {
+                if (item > height)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ programmersCodingTest/ProgrammersDayy20.cs
@@ -0,0 +1,80 @@
+﻿namespace programmersCodingTest
+{
+    internal class ProgrammersDayy20
+    {
+        //프로그래머스 입문 Day20
+        public int solution1(int[,] dots) // 직사각형 넓이 구하기
+        {
+            int answer = 0;
+            for (int i = 3; i >= 0; i--)
+            {
+                dots[i, 0] -= dots[0, 0];
+                dots[i, 1] -= dots[0, 1];
+                if (dots[i, 0] * dots[i, 1] != 0)
+                {
+                    answer = dots[i, 0] * dots[i, 1];
+                    return (answer < 0) ? answer * -1 : answer;
+                }
+            }
+
+            return 1;
+        }
+        public int[] solution2(string[] keyinput, int[] board) // 캐릭터의 좌표
+        {
+            int[] answer = new int[2];
+            foreach (var item in keyinput)
+            {
+                if (item == "up" && answer[1] < board[1] / 2)
+                    answer[1]++;
+                if (item == "down" && answer[1] > -board[1] / 2)
+                    answer[1]--;
+                if (item == "right" && answer[0] < board[0] / 2)
+                    answer[0]++;
+                if (item == "left" && answer[0] > -board[0] / 2)
+                    answer[0]--;
+            }
+            return answer;
+        }
+        public int solution3(int[] numbers) // 최댓값 만들기 (2)
+        {
+            Array.Sort(numbers);
+            return (numbers[0] * numbers[1] > numbers[numbers.Length - 1] * numbers[numbers.Length - 2]) ? numbers[0] * numbers[1] : numbers[numbers.Length - 1] * numbers[numbers.Length - 2];
+        }
+        public string solution4(string polynomial) // 다항식 더하기
+        {
+            string[] answer = polynomial.Split(' ');
+            int x = 0;
+            int s = 0;
+            string temp = "";
+
+            foreach (var item in answer)
+            {
+                if (int.TryParse(item, out int a))
+                {
+                    s += a;
+                }
+                else if (item == "x")
+                    x++;
+                else
+                {
+                    for (int i = 0; i < item.Length - 1; i++)
+                    {
+                        temp += item[i];
+                    }
+                    int.TryParse(temp, out a);
+                    x += a;
+                    temp = "";
+                }
+            }
+            if (x == 1 && s == 0)
+                return "x";
+            if (x == 0)
+                return $"{s}";
+            if (s == 0)
+                return $"{x}x";
+            if (x == 1)
+                return $"x + {s}";
+            return $"{x}x + {s}";
+        }
+    }
+}
