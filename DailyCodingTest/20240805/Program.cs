ccea5b81f69a76ab2e68bf86df51345d329db954
februarywind <89524114+februarywind@users.noreply.github.com>
2024-08-05 16:30:10 +0900

    20240805
    오늘 푼 문제

---
+++ DailyCodingTest/20240805/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _20240805
+{
+    internal class Program
+    {
+        /// <summary>
+        /// name : 푸드 파이트 대회
+        /// link : https://school.programmers.co.kr/learn/courses/30/lessons/134240
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] food = { 1, 3, 4, 6 };
+            Console.WriteLine(solution(food));
+        }
+
+        public static string solution(int[] food)
+        {
+            string answer = "";
+            for (int i = 1; i < food.Length; i++)
+            {
+                for (int j = 0; j < food[i] / 2; j++)
+                {
+                    answer += i;
+                }
+            }
+            char[] chars = answer.ToCharArray();
+            Array.Reverse(chars);
+            answer += '0' + new string(chars);
+            return answer;
+        }
+    }
+}
---
+++ DailyCodingTest/20240805/README.md
@@ -0,0 +1,3 @@
+# codingTest
+ codingTest
+ 마크업 언어는 어려워
