2f0b0b5bc5cd1ffa8ea63d4a4ee84307d83118f7
februarywind <89524114+februarywind@users.noreply.github.com>
2024-08-05 17:56:38 +0900

    0805-2
    옹알이 문제 풀기

---
+++ DailyCodingTest/20240805-2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _20240805_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = { "ayaye", "uuu", "yeye", "yemawoo", "ayaayaa" };
+            solution(a);
+        }
+
+        public static int solution(string[] babbling)
+        {
+            int answer = 0;
+            string[] ongal = { "aya", "ye", "woo", "ma" };
+            for (int i = 0; i < babbling.Length; i++)
+            {
+                List<string> list = new List<string>();
+                string temp = "";
+                foreach (var item in babbling[i])
+                {
+                    temp += item;
+                    if (Array.IndexOf(ongal, temp) != -1)
+                    {
+                        if (!(list.Count > 0 && list[list.Count - 1] == temp))
+                        {
+                            list.Add(temp);
+                            temp = "";
+                        }
+                    }
+                }
+                temp = "";
+                foreach (var item in list)
+                {
+                    temp += item;
+                }
+                if (babbling[i] == temp)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
