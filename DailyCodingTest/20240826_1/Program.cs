1e33309dd7e6d86493887683edd85c5241276d3b
februarywind <89524114+februarywind@users.noreply.github.com>
2024-08-26 18:22:13 +0900

    0826

---
+++ DailyCodingTest/20240826_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _20240826_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            solution(5000);
+        }
+        public static int solution(int n)
+        {
+            int answer = 1;
+
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n--;
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
