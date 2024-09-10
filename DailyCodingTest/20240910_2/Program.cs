c706271befe416ef12fe7038b8df04745156a2a4
februarywind <89524114+februarywind@users.noreply.github.com>
2024-09-10 17:45:01 +0900

    0910

---
+++ DailyCodingTest/20240910_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _20240910_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            int[] nm = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+            int[] card = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+
+            for (int i = 0; i < card.Length - 2; i++)
+                for (int j = i + 1; j < card.Length - 1; j++)
+                    for (int k = j + 1; k < card.Length; k++)
+                    {
+                        int temp = card[i] + card[j] + card[k];
+                        if (temp > nm[1])
+                            continue;
+                        answer = Math.Max(answer, temp);
+                        if (answer == nm[1])
+                            card = new int[0] { };
+                    }
+            Console.WriteLine(answer);
+        }
+    }
+}
