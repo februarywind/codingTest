d79107650a87291e2ee9fd8c70f3bead2c1c3006
februarywind <februarywind0@gmail.com>
2024-09-21 14:21:35 +0900

    0921

---
+++ DailyCodingTest/20240921_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _20240921_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()) * 2;
+            List<int> list = Enumerable.Range(1, 14).ToList();
+            for (int i = 0; i < 14; i++)
+            {
+                list.Add(1);
+                for (int j = 0; j < 13; j++)
+                {
+                    list.Add(list.Last() + list[list.Count - 14]);
+                }
+            }
+            List<int> temp = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                temp.Add(int.Parse(Console.ReadLine()));
+                if (temp.Count == 2)
+                {
+                    Console.WriteLine(list[temp[0] * 14 + temp[1] - 1]);
+                    temp.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ DailyCodingTest/20240921_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace _20240921_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            Queue<int> q = new Queue<int>();
+
+            int n = int.Parse(sr.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string s = sr.ReadLine();
+                switch (s[0]) 
+                {
+                    case 'p':
+                        if (s[1] == 'o')
+                            sw.WriteLine(q.Count > 0 ? q.Dequeue() : -1);
+                        else
+                            q.Enqueue(int.Parse(s.Split()[1]));
+                        break;
+                    case 's':
+                        sw.WriteLine(q.Count);
+                        break;
+                    case 'e':
+                        sw.WriteLine(q.Count == 0 ? 1 : 0);
+                        break;
+                    case 'f':
+                        sw.WriteLine(q.Count != 0 ? q.Peek() : -1);
+                        break;
+                    case 'b':
+                        sw.WriteLine(q.Count != 0 ? q.Last() : -1);
+                        break;
+                }
+            }
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
